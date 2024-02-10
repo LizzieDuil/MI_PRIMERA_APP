aa91a851be37bbf526db6b237d83f72ad326e2ab
Pc09 <pc09@iMac-de-Pc09.local>
2024-02-10 10:46:22 -0600

    primer commit

---
+++ src/MyComponent.js
@@ -0,0 +1,11 @@
+export function MyComponent()
+{
+  return <h1>este es mi componente</h1>
+}
+export function Presentacion() 
+{
+  return <div><h1>Mi nombre es Lizdebeth </h1>
+  <h3>Mi edad es 41 anios</h3>
+  <p>Trabajo cauterizando</p>
+  </div> 
+}
